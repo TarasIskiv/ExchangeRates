552360661069de43d4dbcfdc06082a11a22e5973
Taras <taras.iskiv@gmail.com>
2021-07-22 01:04:38 +0300

    Changes in Model.

--- Models/Rates.cs
+++ Models/Rates.cs
@@ -14,28 +14,34 @@ namespace ExchangeRates.Models
 
         private void gettingExchangeRatesFromWebPage()
         {
-            //HtmlAgilityPack.HtmlWeb htmlAgility = new HtmlAgilityPack.HtmlWeb();
-            //HtmlAgilityPack.HtmlDocument document = htmlAgility.Load("https://finance.i.ua/");
-            //foreach (var item in document.DocumentNode.SelectNodes("//div[@class='data_container']/table[@class='table table-data -important']/tbody/tr/td/span/span"))
-            //{
-            //    Console.WriteLine(item.InnerText);
-            //}
-            //Console.WriteLine();
-            //var mon = new List<string>();
-            //foreach (var ite_ in document.DocumentNode.SelectNodes("//div[@class='data_container']/table[@class='table table-data -important']/tbody/tr/th"))
-            //{
-            //    mon.Add(ite_.InnerText.ToString());
-            //    Console.WriteLine(ite_.InnerText);
-            //}
-            //var al = mon.Distinct();
-            //foreach (var i in al)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            HtmlAgilityPack.HtmlWeb htmlAgility = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = htmlAgility.Load("https://finance.i.ua/");
+
+            foreach (var item in document.DocumentNode.SelectNodes("//div[@class='data_container']/table[@class='table table-data -important']/tbody/tr/td/span/span"))
+            {
+                ratesValues.Add(Convert.ToDecimal(item.InnerText));
+            }
+            var tempRatesValues = new List<decimal>();
+            for (int i = 0; i < ratesValues.Count; i += 5)
+            {
+                if(i != 0)
+                {
+                    tempRatesValues.Add(ratesValues[i]);
+                }
+            }
+            ratesValues = tempRatesValues;
+            var tempAllRates = new List<string>();
+            foreach (var ite_ in document.DocumentNode.SelectNodes("//div[@class='data_container']/table[@class='table table-data -important']/tbody/tr/th"))
+            {
+                tempAllRates.Add(ite_.InnerText.ToString());
+                Console.WriteLine(ite_.InnerText);
+            }
+            rates = tempAllRates.Distinct().ToList();
+            
         }
         public Rates()
         {
-
+            gettingExchangeRatesFromWebPage();
         }
     }
 }
