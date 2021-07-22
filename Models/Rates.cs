ec030b9b565b9105bfb2bbf09d8eff7598ee388f
Taras <taras.iskiv@gmail.com>
2021-07-22 19:12:08 +0300

    Working Model, binded values to MVVM.

--- Models/Rates.cs
+++ Models/Rates.cs
@@ -8,40 +8,65 @@ namespace ExchangeRates.Models
 {
     internal class Rates
     {
-        private List<string> rates;
+        internal Dictionary<string, decimal> rateAndValue;
 
-        private List<decimal> ratesValues;
-
-        private void gettingExchangeRatesFromWebPage()
+        private Dictionary<string, decimal> gettingExchangeRatesFromWebPage()
         {
             HtmlAgilityPack.HtmlWeb htmlAgility = new HtmlAgilityPack.HtmlWeb();
             HtmlAgilityPack.HtmlDocument document = htmlAgility.Load("https://finance.i.ua/");
 
+            List<string> rates = new List<string>();
+
+            List<string> ratesValues = new List<string>();
             foreach (var item in document.DocumentNode.SelectNodes("//div[@class='data_container']/table[@class='table table-data -important']/tbody/tr/td/span/span"))
             {
-                ratesValues.Add(Convert.ToDecimal(item.InnerText));
+                ratesValues.Add(item.InnerText);
             }
-            var tempRatesValues = new List<decimal>();
-            for (int i = 0; i < ratesValues.Count; i += 5)
+            var t = new List<string>();
+            for (int i = 0; i < 24; ++i)
             {
-                if(i != 0)
-                {
-                    tempRatesValues.Add(ratesValues[i]);
-                }
+                t.Add(ratesValues[i]);
             }
-            ratesValues = tempRatesValues;
+
+            ratesValues.Clear();
+
+            for (int i = 4; i < t.Count; i += 6)
+            {
+                ratesValues.Add(t[i]);
+            }
+
             var tempAllRates = new List<string>();
             foreach (var ite_ in document.DocumentNode.SelectNodes("//div[@class='data_container']/table[@class='table table-data -important']/tbody/tr/th"))
             {
                 tempAllRates.Add(ite_.InnerText.ToString());
-                Console.WriteLine(ite_.InnerText);
+
             }
             rates = tempAllRates.Distinct().ToList();
-            
+
+            var ratesValuesDecimal = new List<decimal>();
+
+
+            for (int i = 0; i < ratesValues.Count; ++i)
+            {
+                var elements = ratesValues[i].Split('.');
+                decimal val = Convert.ToDecimal(elements[0]);
+                val += Convert.ToDecimal(elements[1]) / 10000;
+                ratesValuesDecimal.Add(val);
+            }
+
+            var tempDictionary = new Dictionary<string, decimal>();
+
+            for (int i = 0; i < ratesValues.Count; ++i)
+            {
+                tempDictionary.Add(rates[i], ratesValuesDecimal[i]);
+            }
+
+            return tempDictionary;
+
         }
         public Rates()
         {
-            gettingExchangeRatesFromWebPage();
+           rateAndValue = gettingExchangeRatesFromWebPage();
         }
     }
 }
--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -8,6 +8,36 @@ namespace ExchangeRates.ViewModels
 {
     internal class MainPageViewModel : BaseViewModel.ViewModel
     {
+        #region Exchange Rates
+        private List<string> rates;
+        private List<decimal> ratesValues;
 
+        public List<string> Rates 
+        { 
+            get => rates;
+            set => Set(ref rates, value);
+        }
+        public List<decimal> RatesValues 
+        {
+            get => ratesValues;
+            set => Set(ref ratesValues, value);
+        }
+
+        public MainPageViewModel()
+        {
+            Models.Rates model = new Models.Rates();
+            var directoryWithDataFromModel = model.rateAndValue;
+            var tempRates = new List<string>();
+            var tempRatesValues = new List<decimal>();
+            foreach (var item in directoryWithDataFromModel)
+            {
+                tempRates.Add(item.Key);
+                tempRatesValues.Add(item.Value);
+            }
+
+            Rates = tempRates;
+            RatesValues = tempRatesValues;
+        }
+        #endregion
     }
 }
